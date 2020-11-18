03ab8344176a6e3f771f7bfa730c9f76c68a956e
beenishsa <71415197+beenishsa@users.noreply.github.com>
2020-11-17 20:20:04 -0500

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-$('#search-button').on('click', () => {
-    console.log("i was clicked");
+//$('#fetch-from-api').click(function() {
+    //console.log("i was clicked");
     
-       $.get("https://api.giphy.com/v1/gifs/search?api_key=qZqq8jPd45n49RvLiY6PtNPiAixu1RsK&q=corona", function(giphyResponse) {
-         console.log(giphyResponse);
-         console.log('gipgyData', giphyResponse.data)
+$.get("https://api.giphy.com/v1/gifs/search?api_key=qZqq8jPd45n49RvLiY6PtNPiAixu1RsK&q=corona", function(giphyResponse) {
+    console.log(giphyResponse);
+    console.log('giphyData', giphyResponse.data)
+    console.log('giphyResponse result 0 (1st result)'- giphyResponse.data[0]);
+    console.log('giphyResponse result 0 (1st result) - all images list', giphyResponse.data[0].images);
+    console.log('giphyResponse result 0 (1st result)- all images list- original image', giphyResponse.data[0].images.original);
+    console.log('giphyResponse result 0 (1st result) - all images list- original image-url', giphyResponse.data[0].images.original.url);
+     
+    let imgHtml="<img src=${giphyResponse.data[0].images.original.url} />"
+
+    $('#first-result-image').html(imgHtml);
+
+
         });
     })
-});
+//});
 
 
 
